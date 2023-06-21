e0820a3170a5911368a285a990e4b10234ee86ea
Bella Bekanova <108465697+BellaBekanova@users.noreply.github.com>
2023-06-21 15:17:49 +0100

    the contact modal is styled

--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -5,12 +5,12 @@ export default function ContactModal() {
   return (
     <main className="flex flex-col flex-1 items-center overflow-y-auto text-xl">
       <header className="flex justify-around">
-        <div className="flex flex-col mr-5">
+        <div className="flex flex-col m-5 justify-around pb-10">
           <Image src="/phone.png" alt="Profile Image" width={200} height={200} />
           <p>Web Developer</p>
           <p>London, UK</p>
         </div>
-        <div className="flex flex-col ml-5">
+        <div className="flex flex-col m-5">
           <div>
             <p>4</p>
             <p>Projects Completed</p>
@@ -28,25 +28,33 @@ export default function ContactModal() {
           </div>
         </div>
       </header>
-      <article className="ml-15 mr-15 mt-30 mb-30">
+      <article className="flex p-5">
         <p>I conquered the fashion industry for 10 years and now I code for fun and build useful tools for businesses around the world.</p>
       </article>
       <footer>
-        <div className="flex flex-col lg:flex-row m-15">
+        <div className="flex flex-col lg:flex-row mb-5">ç
           <div className="flex justify-around">
             <Link href="tel:+1234567890">
-                <Image src="/phone.png" alt="Phone" width={24} height={24} />
+              <div className="m-8">
+                <Image src="/phone.png" alt="Phone" width={64} height={64} />
+              </div>
             </Link>
             <Link href="mailto:your-email@example.com">
-                <Image src="/email.png" alt="Email" width={24} height={24} />
+              <div className="m-8">
+                <Image src="/email.png" alt="Email" width={64} height={64} />
+              </div>
             </Link>
           </div>
           <div className="flex justify-around">
             <Link href="https://github.com/your-username">
-                <Image src="/github-sign.png" alt="GitHub" width={24} height={24} />
+              <div className="m-8">
+                <Image src="/github-sign.png" alt="GitHub" width={64} height={64} />
+              </div>
             </Link>
             <Link href="https://www.linkedin.com/in/your-username">
-                <Image src="/linkedin.png" alt="LinkedIn" width={24} height={24} />
+              <div className="m-8">
+                <Image src="/linkedin.png" alt="LinkedIn" width={64} height={64} />
+              </div>
             </Link>
           </div>
         </div>
